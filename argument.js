336b7f799987a5667bd71938bde065aef314ba6e
Salah Aboker <salahcodeskills@outlook.com>
2018-11-04 20:19:22 -0800

    Add to my new js file

---
+++ argument.js
@@ -0,0 +1,14 @@
+// function square(num) {
+//     console.log(num * num);
+// }
+// square(3);
+
+// function sayHello(name) {
+//     console.log("Hello there " + name + "!");
+// }
+// sayHello("Salah");
+
+// function area(length, width) {
+//     console.log(length - width);
+// }
+// area(19, 6);
