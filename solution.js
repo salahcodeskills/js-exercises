90c7ef723eeeb2d6819876e89b79d491a2278b70
Salah Aboker <salahcodeskills@outlook.com>
2018-11-05 06:56:29 -0800

    updated solution file

--- solution.js
+++ solution.js
@@ -6,6 +6,18 @@
 //     }
 // }
 
-function isEven(num) {
-    return nun % 2 === 0;
+// function isEven(num) {
+//     return nun % 2 === 0;
+// }
+
+function factorial(num) {
+    //define a result variable
+    let result = 1;
+    //calculate factorial and store value in result
+    for (let i = 2; i <= num; i++) {
+        result *= i;
+    }
+    //return the result variable
+    return result;
 }
+//factorial (4) 4 x 3 x 2 x 1
